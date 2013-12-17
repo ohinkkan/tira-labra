207f8dbfb8445b6d853e1446a01638dc487242f3
Otto Hinkkanen <otto.hinkkanen@helsinki.fi>
2013-12-17 19:41:31 +0200

    pohjat projektille...

---
+++ Tacticulous/src/main/java/tacticulous/tacticulous/App.java
@@ -0,0 +1,13 @@
+package tacticulous.tacticulous;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Murder! Death! Kill!" );
+    }
+}
